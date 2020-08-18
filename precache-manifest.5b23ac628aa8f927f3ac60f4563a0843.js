cf5a35b227f50077cb21bb4691c4e31119b7c5df
sammer119 <sammer119@users.noreply.github.com>
2020-08-18 16:35:15 +0100

    Initial commit

---
+++ precache-manifest.5b23ac628aa8f927f3ac60f4563a0843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f07b314c052907011bfb8f0b8fe961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2287f3ecc41a8bc4ba9",
+    "url": "/static/css/main.09f3c54e.chunk.css"
+  },
+  {
+    "revision": "c6030c45a8bbad5f0db7",
+    "url": "/static/js/2.b6a5d628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6a5d628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2287f3ecc41a8bc4ba9",
+    "url": "/static/js/main.404ae7bd.chunk.js"
+  },
+  {
+    "revision": "34b8440f7da5f4276088",
+    "url": "/static/js/runtime-main.560e638a.js"
+  }
+]);
